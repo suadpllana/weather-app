cfd31ea97df1cd0d9b53514599f66b5c0b2837ad
suadpllana <suadpllana14@gmail.com>
2024-09-30 01:25:50 +0200

    first commit

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,24 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Weather() {
-    const [weatherData, setWeatherData] = useState(null);
-    const [error, setError] = useState(null);
+    const [weatherData, setWeatherData] = useState()
+    const [error, setError] = useState();
+
+    const inputRef = useRef(null)
 
     async function displayWeather() {
-        const APIKEY = 'eb3c88cf69e595751d0eae9e1cefd0b9'; 
-        const cityName = document.getElementById('cityName').value;
+        const APIKEY = import.meta.env.VITE_API_KEY; 
+ 
 
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}`;
-            const response = await fetch(url);
-            
-            if (!response.ok) {
-                throw new Error('City not found');
+            if(!inputRef.current.value){
+                return
             }
             
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&appid=${APIKEY}`;
+            const response = await fetch(url);
+
+
+            if(!response.ok){
+                throw new Error("Something went wrong")
+            }
+            
+          
             const data = await response.json();
             setWeatherData(data);
             setError(null); 
+            inputRef.current.value = ``
+         
         } catch (err) {
             setWeatherData(null);
             setError(err.message);
@@ -26,19 +36,46 @@ function Weather() {
     }
 
     const kelvinToCelsius = (temp) => (temp - 273.15).toFixed(2);
+           
+        function enter(e){
+            if(e.key === "Enter"){
+                displayWeather()
+            }
+        }
+      
+        function emoji(temp) {
+            if (temp > 28) {
+                return <p>☀️</p>;
+            } else if (temp <= 28 && temp >= 18) {
+                return <p>⛅</p>;
+            } else if (temp < 18 && temp >= 12) {
+                return <p>☁️</p>;
+            } else if (temp < 12 && temp >= 8) {
+                return <p>⛈️</p>;
+            } else if (temp < 8) {
+                return <p>❄️</p>;
+            } 
+            return null;
+        }
+    
+      
+    
+       
 
     return (
         <div className="container">
             <h1>Weather App</h1>
-            <input type="text" id="cityName" />
+            <input onKeyDown={(e) => enter(e)} type="text" ref={inputRef}/>
             <button onClick={displayWeather}>Search</button>
             {error && <p>Error: {error}</p>}
             {weatherData && !error && (
                 <>
                     <p>City: {weatherData.name}</p>
-                    <p>Temperature: {kelvinToCelsius(weatherData.main.temp)}°C</p>
+                    <p>Temperature: { kelvinToCelsius(weatherData.main.temp)}°C</p>
                     <p>Humidity: {weatherData.main.humidity}%</p>
                     <p>{weatherData.weather[0].description}</p>
+                
+                    <h1 className="emoji">{emoji(kelvinToCelsius(weatherData.main.temp))}</h1>
                 </>
             )}
         </div>
